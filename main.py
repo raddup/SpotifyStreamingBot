8a1b3815aa9fec292b05aa3e28836028f4913645
thisisawesome1994 <58063460+thisisawesome1994@users.noreply.github.com>
2021-03-07 19:42:06 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -176,16 +176,18 @@ class Main:
             logged_in = False
 
             driver.get('https://accounts.spotify.com/en/login/')
-            login_username_present = EC.presence_of_element_located((By.ID, 'login-username'))
-            WebDriverWait(driver, self.website_load_max_wait).until(login_username_present)
-
-            username_elem = driver.find_element_by_id('login-username').send_keys(username)
-            password_elem = driver.find_element_by_id('login-password').send_keys(password)
-            login_button_elem = driver.find_element_by_id('login-button').click()
+            element_present = EC.presence_of_element_located((By.ID, 'login-username'))
+            WebDriverWait(driver, self.website_load_max_wait).until(element_present)
+            username_elem = driver.find_element_by_id('login-username')
+            username_elem.send_keys(username)
+            password_elem = driver.find_element_by_id('login-password')
+            password_elem.send_keys(password)
+            login_button_elem = driver.find_element_by_id('login-button')
+            login_button_elem.click()
 
             try:
-                url_to_be_present = EC.url_to_be('https://accounts.spotify.com/en/status')
-                WebDriverWait(driver, self.login_check_max_wait).until(url_to_be_present)
+                element_present = EC.url_to_be('https://accounts.spotify.com/en/status')
+                WebDriverWait(driver, self.login_check_max_wait).until(element_present)
                 self.PrintText(Fore.CYAN,Fore.RED,'LOGIN',f'LOGGED IN WITH | {username}:{password}')
                 logged_in = True
             except TimeoutException:
@@ -265,16 +267,20 @@ class Main:
 
             if self.Login(username,password,driver) == True:
                 driver.get(self.url)
-                element_present = EC.presence_of_element_located((By.XPATH, '/html/body/div[4]/div/div[2]/div[4]/main/div/div[2]/div/div/div[2]/section/div[4]/div/div[2]/div[2]/div[1]/div/div/div[1]'))
-                WebDriverWait(driver, self.max_wait).until(element_present)
+                element_present_cookies = EC.presence_of_element_located((By.XPATH, '//*[@id="onetrust-close-btn-container"]/button'))
+                WebDriverWait(driver, self.max_wait).until(element_present_cookies)
+                WebDriverWait(driver,self.max_wait).until(EC.element_to_be_clickable((By.XPATH,f'//*[@id="onetrust-close-btn-container"]/button'))).click()
+                #element_present = EC.presence_of_element_located((By.XPATH, '//*[@id="main"]/div/div[2]/div[3]/main/div[2]/div[2]/div/div/div[2]/section/div[2]/div[2]/div/button[1]'))
+                #WebDriverWait(driver, self.max_wait).until(element_present)
+                WebDriverWait(driver,self.max_wait).until(EC.element_to_be_clickable((By.XPATH,f'//*[@id="main"]/div/div[2]/div[3]/main/div[2]/div[2]/div/div/div[2]/section/div[2]/div[2]/div/button[1]'))).click()
                 index = 0
                 for i in range(self.number_of_songs):
                     index += 1
                     playtime = randint(self.minplay,self.maxplay)
-                    WebDriverWait(driver,self.max_wait).until(EC.element_to_be_clickable((By.XPATH,f'/html/body/div[4]/div/div[2]/div[4]/main/div/div[2]/div/div/div[2]/section/div[4]/div/div[2]/div[2]/div[{index}]/div/div/div[1]'))).click()
-                    WebDriverWait(driver,self.max_wait).until(EC.text_to_be_present_in_element((By.XPATH,'/html/body/div[4]/div/div[2]/div[3]/footer/div/div[2]/div/div[2]/div[1]'),'0:01'))
+                    WebDriverWait(driver,self.max_wait).until(EC.text_to_be_present_in_element((By.XPATH,'//*[@id="main"]/div/div[2]/div[2]/footer/div/div[2]/div/div[2]/div[1]'),'0:01'))
                     sleep(playtime)
                     self.PrintText(Fore.CYAN,Fore.RED,'PLAYLIST OR ALBUM STREAM',f'SONG {index} | STREAMED FOR {playtime}s | WITH {username}:{password}')
+                    WebDriverWait(driver,self.max_wait).until(EC.element_to_be_clickable((By.XPATH,f'//*[@id="main"]/div/div[2]/div[2]/footer/div/div[2]/div/div[1]/button[4]'))).click()
         except:
             driver.quit()
             self.StreamPlaylistOrAlbum(username,password)
